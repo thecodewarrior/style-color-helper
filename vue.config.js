e76eed46379f66c81f09321b78b6c53db1cba22a
Pierce Corcoran <code@thecodewarrior.dev>
2021-07-08 11:12:49 -0700

    Fix Vue source maps

---
+++ vue.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  chainWebpack: config => {
+    config.devtool('source-map');
+    config.output.devtoolModuleFilenameTemplate(info => {
+      let filename = `webpack:///${info.resourcePath}?${info.hash}`
+      if(info.resourcePath.startsWith('./src')) {
+        filename = 'src://' + info.resourcePath.substring(2);
+      }
+      return filename;
+    })
+
+    config.output.devtoolFallbackModuleFilenameTemplate = 'webpack://[resource-path]?[hash]'
+  }
+}
