d2d7bd240a125a80113f1be707e16e5c520e40f3
Pierce Corcoran <code@thecodewarrior.dev>
2021-07-11 15:24:25 -0700

    good god source maps
    https://github.com/vuejs/vue-loader/issues/146#issuecomment-877869882

--- vue.config.js
+++ vue.config.js
@@ -1,14 +1,39 @@
 module.exports = {
   chainWebpack: config => {
     config.devtool('source-map');
-    config.output.devtoolModuleFilenameTemplate(info => {
-      let filename = `webpack:///${info.resourcePath}?${info.hash}`
-      if(info.resourcePath.startsWith('./src')) {
-        filename = 'src://' + info.resourcePath.substring(2);
-      }
-      return filename;
-    })
 
-    config.output.devtoolFallbackModuleFilenameTemplate = 'webpack://[resource-path]?[hash]'
+    /**
+     * Sorts the mess of source maps.
+     */
+    config.output.devtoolModuleFilenameTemplate(info => {
+      // This is what I used to determine the rules below. It outputs a ton of information so you can figure out how to
+      // distinguish between the files you want and the ones you don't.
+      // return `webpack://${info.resourcePath}?path=${info.resourcePath},query=${info.query},loaders=${info.allLoaders},hash=${info.hash}`;
+
+      if(info.resourcePath.endsWith('.vue')) { // do some special processing for .vue files
+
+        // the <script> block from vue files. this is where you put script breakpoints.
+        if (
+            info.query.startsWith('?vue&type=script') && // only the script section
+            !info.allLoaders.includes('babel')           // don't use the one from babel
+        ) {
+          return `src://${info.resourcePath}?script`;
+        }
+
+        // the full vue file. You can't put script breakpoints here, but you can put render breakpoints here.
+        if (
+            info.query === '' && info.allLoaders === '' // no extra processing? I dunno, but it works
+        ) {
+          return `src://${info.resourcePath}?render`;
+        }
+
+      } else if(
+          info.resourcePath.startsWith('./src') // this path may depend on your project configuration
+      ) {
+        return `src://${info.resourcePath}?${info.hash}`;
+      }
+
+      return `webpack:///${info.resourcePath}?${info.hash}`;
+    });
   }
 }
