41d649a8ed5d441dc4fd06ed8f731fca5e8f4241
Pierce Corcoran <code@thecodewarrior.dev>
2021-07-11 19:42:45 -0700

    Add GH pages publicPath to vue config

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,7 @@
 module.exports = {
+  publicPath: process.env.NODE_ENV === 'production'
+      ? '/style-color-helper/'
+      : '/',
   chainWebpack: config => {
     config.devtool('source-map');
 
