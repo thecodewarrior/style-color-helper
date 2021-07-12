fa19073d3b923d012f40e9a638b8461ec91f0ce6
Pierce Corcoran <code@thecodewarrior.dev>
2021-07-11 19:40:58 -0700

    Remove typescript build outputs

--- src/utils.js
+++
@@ -1,8 +0,0 @@
-export function clamp(min, value, max) {
-    if (value < min)
-        return min;
-    if (value > max)
-        return max;
-    return value;
-}
-//# sourceMappingURL=utils.js.map
