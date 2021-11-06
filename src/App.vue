ba66a158a460b915013949e10f56aade3e16fbd2
Pierce Corcoran <code@thecodewarrior.dev>
2021-11-05 20:48:39 -0700

    Improve sticky spectrum

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,36 @@
 <template>
   <div class="app">
-    <color-panel class="main-panel" :model="model" :hide-filters="hideFilters" :hide-main-spectrum="true"/>
-    <div class="sticky-block">
-      <color-picker-spectrum
-          class="sticky-spectrum"
-          hue="x" :saturation="model.saturation" lightness="-y"
-          v-model:x="hueAxis" v-model:y="inverseLightness"
-          :render-width="300" :render-height="100"
-          :model="model"
-          :hide-filters="hideFilters"
-          :border="{radius: 20, width: 5, transition: '0.5s'}"
-      />
-      <div class="swatch raw-swatch" :style="rawSwatchStyle">{{ model.rawColor.hex() }}</div>
-      <div class="swatch filtered-swatch" :style="filteredSwatchStyle">{{ filteredColor.hex() }}</div>
-      <div class="sticky-separator"></div>
-    </div>
-    <div class="sticky-mask"/>
-    <div class="panel-separator"></div>
-    <filter-panel :model="model" v-model:hide-filters="hideFilters"/>
+    <color-component
+        :model="model" component="hue" label="H" :hide-filters="hideFilters"
+        style="grid-area: hue; z-index: 10;"
+    />
+    <color-component
+        :model="model" component="saturation" label="S" :hide-filters="hideFilters"
+        style="grid-area: sat; z-index: 10;"
+    />
+    <color-component
+        :model="model" component="lightness" label="L" :hide-filters="hideFilters"
+        style="grid-area: lit; z-index: 10;"
+    />
+    <color-picker-spectrum
+        class="main-spectrum"
+        hue="x" :saturation="model.saturation" lightness="-y"
+        v-model:x="model.normalHue" v-model:y="model.lightness"
+        :render-width="300" :render-height="100"
+        :model="model"
+        :hide-filters="hideFilters"
+        :border="{radius: 20, width: 5, transition: '0.5s'}"
+        style="grid-area: main; z-index: 12;"
+    />
+    <color-swatch class="small-swatch" style="grid-area: orig; z-index: 8;" :color="model.rawColor"/>
+    <color-swatch class="small-swatch" style="grid-area: filt; z-index: 8;" :color="filteredColor"/>
+    <color-edit-swatch class="main-swatch" style="grid-area: orig; z-index: 10;" v-model:color="model.rawColor"/>
+    <color-swatch class="main-swatch" style="grid-area: filt; z-index: 10;" :color="filteredColor"/>
+
+    <div class="sticky-top-mask" style="z-index: 11;"/> <!-- sticks up above the main spectrum-->
+    <div class="sticky-bottom-mask" style="z-index: 5;"/> <!-- covers the space between the spectrum and separator -->
+    <div class="panel-separator" style="z-index: 10;"></div>
+    <filter-panel class="side-panel" :model="model" v-model:hide-filters="hideFilters"/>
   </div>
 </template>
 
@@ -30,9 +43,12 @@ import ColorPanel from "@/components/ColorPanel.vue";
 import FilterPanel from "@/components/FilterPanel.vue";
 import ColorPickerSpectrum from "@/components/ColorPickerSpectrum.vue";
 import {swatchStyle} from "@/utils";
+import ColorComponent from "@/components/ColorComponent.vue";
+import ColorEditSwatch from "@/components/ColorEditSwatch.vue";
+import ColorSwatch from "@/components/ColorSwatch.vue";
 
 @Options({
-  components: {ColorPickerSpectrum, FilterPanel, ColorPanel},
+  components: {ColorSwatch, ColorEditSwatch, ColorComponent, ColorPickerSpectrum, FilterPanel, ColorPanel},
   props: {}
 })
 export default class App extends Vue {
@@ -42,32 +58,8 @@ export default class App extends Vue {
     this.model.addFilter(new ParameterizedFilter(filterRegistry["posterize"])).values[0] = 3
   }
 
-  get hueAxis(): number {
-    return this.model.hue / 360
-  }
-
-  set hueAxis(value: number) {
-    this.model.hue = value * 360
-  }
-
-  get inverseLightness(): number {
-    return 1 - this.model.lightness
-  }
-
-  set inverseLightness(value: number) {
-    this.model.lightness = 1 - value
-  }
-
-  get rawSwatchStyle() {
-    return swatchStyle(this.model.rawColor)
-  }
-
   get filteredColor() {
     return this.hideFilters ? this.model.rawColor : this.model.computedColor
-  }
-
-  get filteredSwatchStyle() {
-    return swatchStyle(this.filteredColor)
   }
 }
 </script>
@@ -116,9 +108,15 @@ body {
 <style scoped>
 .app {
   display: grid;
-  grid-template-columns: 300px 30px 300px;
-  grid-template-rows: 300px;
-  grid-template-areas: "main sep filters";
+
+  grid-template-rows: 35px 35px 35px 130px 65px;
+  grid-template-columns: 150px 15px 150px 30px 300px;
+  grid-template-areas:
+      "hue  hue  hue  sep side"
+      "sat  sat  sat  sep side"
+      "lit  lit  lit  sep side"
+      "main main main sep side"
+      "orig  ..  filt sep side";
   padding: 20px;
 
   background: var(--main-background);
@@ -135,27 +133,41 @@ body {
 
   margin: -10px 0;
   justify-self: center;
-  z-index: 2;
 }
 
-.main-panel {
-  grid-area: main;
-  z-index: 2;
+.side-panel {
+  grid-area: side;
 }
 
-.sticky-mask {
+.sticky-bottom-mask, .sticky-top-mask, .small-swatch {
   display: none;
 }
 
-.sticky-block {
-  display: none;
+.main-spectrum {
+  margin-bottom: 10px;
+  border: var(--standard-border);
+}
+
+.main-swatch {
+  padding: 15px 0;
+  font-size: 20px;
+  border: var(--standard-border);
+  border-radius: 20px;
 }
 
 @media (max-width: 700px) {
   .app {
-    grid-template-columns: auto;
-    grid-template-rows: 300px 29px auto;
-    grid-template-areas: "main" "sep" "filters";
+    grid-template-rows: 35px 35px 35px 130px 65px 30px auto;
+    grid-template-columns: 150px 15px 150px;
+    grid-template-areas:
+      "hue  hue  hue "
+      "sat  sat  sat "
+      "lit  lit  lit "
+      "main main main"
+      "orig  ..  filt"
+      "sep  sep  sep "
+      "side side side";
+
     max-height: 100vh;
     overflow-y: scroll;
     -webkit-scrollbar-color: var(--standard-scroll-color);
@@ -164,7 +176,26 @@ body {
     scrollbar-width: var(--standard-scroll-width);
   }
 
+
+  .main-spectrum {
+    position: sticky;
+    top: -15px;
+  }
+
+  .small-swatch {
+    display: block;
+    position: sticky;
+    top: 110px;
+    height: 20px;
+    margin: 0 5px;
+
+    align-self: center;
+    border-radius: 20px;
+  }
+
   .panel-separator {
+    position: sticky;
+    top: 135px;
     width: auto;
     height: 5px;
     margin: 12px -10px;
@@ -173,60 +204,25 @@ body {
     align-self: center;
   }
 
-  .sticky-mask {
+  .sticky-top-mask {
     display: block;
-    margin: 0 -10px 12px;
-    height: 150px;
-    background: var(--main-background);
-    align-self: end;
-    grid-area: sep;
-    z-index: 1;
-  }
-
-  .sticky-block {
-    display: grid;
-    grid-template-columns: 1fr 1fr;
-    grid-area: sep;
-    grid-template-areas:
-        "main main"
-        "raw filtered"
-        "sep sep";
     position: sticky;
-    margin: 0 -8px 12px;
-    padding: 5px 8px 0;
     top: -20px;
-    align-self: end;
+    height: 50px;
     background: var(--main-background);
-  }
-
-  .sticky-spectrum {
+    align-self: end;
     grid-area: main;
-    height: 100px;
-    border: var(--standard-border);
   }
 
-  .sticky-separator {
+  .sticky-bottom-mask {
+    display: block;
+    position: sticky;
+    top: -20px;
+    margin: 0 -10px 12px;
+    height: 155px;
+    background: var(--main-background);
+    align-self: end;
     grid-area: sep;
-    background-color: var(--main-highlight);
-    border-radius: 3px;
-    width: auto;
-    height: 5px;
-    margin: 0 -10px;
-    justify-self: stretch;
-  }
-
-  .swatch {
-    border-radius: 50px;
-    margin: 5px 15px;
-    text-transform: uppercase;
-    text-align: center;
-  }
-
-  .raw-swatch {
-    grid-area: raw;
-  }
-  .filtered-swatch {
-    grid-area: filtered;
   }
 }
 
